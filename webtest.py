51d266e3748403037de6e8160391790d9efda313
JonSnow <jamison.e.thorne@gmail.com>
2017-10-20 08:10:47 -0500

    Corrected webdriver pathing, made code more human-like

--- webtest.py
+++ webtest.py
@@ -2,20 +2,29 @@ import logging
 import argparse
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
 def main():
     args = parse_args()
-    logging.basicConfig(format='%(asctime)s - %(name)s : %(levelname)s - %(lineno)d: %(message)s', level=logging.DEBUG if args.verbose else logging.INFO)
+    logging.basicConfig(format='%(asctime)s - %(name)s : %(levelname)s - %(lineno)d: %(message)s',
+                        level=logging.DEBUG if args.verbose else logging.INFO)
     
     global logger
     logger = logging.getLogger("webscraper")
     #Change Chrome Driver Path as Needed
-    driver = init_driver('C:/Users/Jon Snow/Documents/chromedriver.exe')
+    #Ubunto Users (see tytus's comments):
+    #https://stackoverflow.com/questions/22476112/using-chromedriver-with-selenium-python-ubuntu
+    driver = init_driver('/home/jonsnow/my_virtualenv/chromedriver-Linux64')
     stock = "AMD"
     Parse(driver, stock)
     driver.close()
+
+
+def init_driver(web_driver_location):
+    driver = webdriver.Chrome(executable_path=web_driver_location)
+    driver.wait = WebDriverWait(driver, 5)
+    return driver
 
 """
 Parse command line arguments
@@ -25,17 +34,6 @@ def parse_args():
     parser.add_argument('-v','--verbose',help='Verbose logging',action="store_true")
     return parser.parse_args()
 
-def init_driver(web_driver_location):
-    driver = webdriver.Chrome(web_driver_location)
-    driver.wait = WebDriverWait(driver, 5)
-    return driver
-'''
-def wait_for_elements(driver, element_location):
-    driver.wait.until(expected_conditions.presence_of_element_located(element_location))
-
-def wait_to_click(driver, click_location):
-    driver.wait.until(expected_conditions.element_to_be_clickable(click_location))
-'''                   
 '''
 Parse driver content for stock data
 >>>Parse(driver, empty)
@@ -44,15 +42,16 @@ Parse driver content for stock data
 def Parse(driver, stock):
     logger.debug("Starting Parse method")
     try:
-        driver.get('http://financials.morningstar.com/ratios/r.html?t={stockvar}'.format(stockvar=stock))
+        driver.get('http://financials.morningstar.com/ratios/r.html?t={stock_var}'.format(stock_var=stock))
     except Exception as e:
-        logger.error("Something bad happened when hitting morningstar: {name} - {error} - %{num}d".format(error=e, name="Jamison",num=10))
+        logger.error("Something bad happened when hitting morningstar: {name} - {error} - %{num}d"
+                     .format(error=e, name="Jamison",num=10))
     logger.debug("URL pinged")
     
     # Operating Cash Flow - Most Recent
-    RecentOperatingCashFlow = driver.find_element_by_xpath('//*[@id="financials"]/table/tbody/tr[22]/td[10]').text
-    
-    logger.info(RecentOperatingCashFlow)
+    WebDriverWait(driver, 10).until(EC.title_contains("from Morningstar.com"))
+    recentoperatingcashflow = driver.find_element_by_xpath('//*[@id="financials"]/table/tbody/tr[22]/td[10]').text
+    logger.info(recentoperatingcashflow)
 
 if __name__ == '__main__':
     main()

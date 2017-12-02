715af8bd669617e4bb16cee01f5921011af26a74
JonSnow <jamison.e.thorne@gmail.com>
2017-12-01 18:07:32 -0600

    grammar corrections

--- piotroski_score.py
+++ piotroski_score.py
@@ -90,8 +90,8 @@ def parse_args():
 """
 parse(selenium.webdriver.chrome.webdriver.WebDriver, str) -> int
 
-navigates through the morningstar financials pages for any given stock financial data and calculates the
-priotroski score.
+navigates through the morningstar financial pages for any given stock financial data and calculates the
+piotroski score.
 
 >>> parse(driver, "AMD")
 4
@@ -114,19 +114,19 @@ def parse(driver, stock):
         #retrieves most recent reported net income
         recent_net_income = float_converter(exception_handling_text_element('//*[@id="financials"]'
                                                                             '/table/tbody/tr[10]/td[10]', driver))
-        #retrieves previous years reported net income
+        #retrieves previous year's reported net income
         previous_net_income = float_converter(exception_handling_text_element('//*[@id="financials"]'
                                                                               '/table/tbody/tr[10]/td[9]', driver))
         #retrieves most recent reported shares
         recent_shares = float_converter(exception_handling_text_element('//*[@id="financials"]'
                                                                         '/table/tbody/tr[18]/td[10]', driver))
-        #retrieves previous years reported shares
+        #retrieves previous year's reported shares
         previous_shares = float_converter(exception_handling_text_element('//*[@id="financials"]'
                                                                           '/table/tbody/tr[18]/td[9]', driver))
         #retrieves most recent reported gross margin
         recent_gross_margin = float_converter(exception_handling_text_element('//*[@id="tab-profitability"]'
                                                                               '/table[1]/tbody/tr[6]/td[10]', driver))
-        #retrieves previous years reported gross margin
+        #retrieves previous year's reported gross margin
         previous_gross_margin = float_converter(exception_handling_text_element('//*[@id="tab-profitability"]'
                                                                                 '/table[1]/tbody/tr[6]/td[9]', driver))
         '''
@@ -135,17 +135,17 @@ def parse(driver, stock):
         #retrieves most recent asset turnover
         recent_asset_turnover = float_converter(exception_handling_text_element('//*[@id="tab-efficiency"]'
                                                                         '/table/tbody/tr[16]/td[10]', driver))
-        #retrieves previous years asset turnover
+        #retrieves previous year's asset turnover
         previous_asset_turnover = float_converter(exception_handling_text_element('//*[@id="tab-efficiency"]'
                                                                           '/table/tbody/tr[16]/td[9]', driver))
         '''
-        #clicks on the financials page which takes you to the income statement
+        #clicks on the financials page, which takes you to the income statement
         human_like_click(driver, wait, '/html/body/div[1]/div[3]/div[1]/div/ul[2]/li[6]/a')
 
         #retrieves the most recent reported revenue
         recent_revenue = float_converter(exception_handling_raw_element('//*[@id="data_i1"]'
                                                                         '/div[@id="Y_5"]', driver))
-        #retrieves previous years reported revenue
+        #retrieves previous year's reported revenue
         previous_revenue = float_converter(exception_handling_raw_element('//*[@id="data_i1"]'
                                                                           '/div[@id="Y_4"]', driver))
         #clicks on the balance sheet page
@@ -153,28 +153,28 @@ def parse(driver, stock):
         #retrieves most recent reported total current assets
         recent_tot_current_assets = float_converter(exception_handling_raw_element('//*[@id="data_ttg1"]'
                                                                                    '/div[@id="Y_5"]', driver))
-        #retrives last years reported total current assets
+        #retrives last year's reported total current assets
         previous_tot_current_assets = float_converter(exception_handling_raw_element('//*[@id="data_ttg1"]'
                                                                                      '/div[@id="Y_4"]', driver))
         #retrieves most recent reported total assets
         recent_total_assets = float_converter(exception_handling_raw_element('//*[@id="data_tts1"]'
                                                                              '/div[@id="Y_5"]', driver))
-        #retrieves previous years reported total assets
+        #retrieves previous year's reported total assets
         previous_total_assets = float_converter(exception_handling_raw_element('//*[@id="data_tts1"]'
                                                                                '/div[@id="Y_4"]', driver))
-        #retrieves previous years previous reported total assets
+        #retrieves previous year's previous reported total assets
         previous_previous_total_assets = float_converter(exception_handling_raw_element('//*[@id="data_tts1"]'
                                                                                         '/div[@id="Y_3"]', driver))
         #retrieves most recent reported total current liabilities
         recent_total_current_liabilities = float_converter(exception_handling_raw_element('//*[@id="data_ttgg5"]'
                                                                                           '/div[@id="Y_5"]', driver))
-        #retrieves previous years reported total current liabilities
+        #retrieves previous year's reported total current liabilities
         previous_total_current_liabilities = float_converter(exception_handling_raw_element('//*[@id="data_ttgg5"]'
                                                                                             '/div[@id="Y_4"]', driver))
         #retrieves most recent reported total liabilities
         recent_long_term_debt = float_converter(exception_handling_raw_element('//*[@id="data_i50"]'
                                                                                '/div[@id="Y_5"]', driver))
-        #retrieves previous years reported total liabilities
+        #retrieves previous year's reported total liabilities
         previous_long_term_debt = float_converter(exception_handling_raw_element('//*[@id="data_i50"]'
                                                                                  '/div[@id="Y_4"]', driver))
         #clicks on the cash flow page
@@ -190,40 +190,40 @@ def parse(driver, stock):
         recent_avg_total_assets = (recent_total_assets + previous_total_assets) / 2
         previous_avg_total_assets = (previous_total_assets + previous_previous_total_assets) / 2
         #Profitability
-        #ROA - current net income / total assets - 1 if pos 0 if neg
+        #ROA - current net income / total assets - 1 if positive, 0 if negative.
         ROA = division_by_zero_check(recent_net_income, recent_total_assets)
         f_score.append(sign_check(ROA))
-        #CFROA  - Cash Flow from operations / tot assets - 1 if pos 0 if neg
+        #CFROA  - Cash Flow from operations / tot assets - 1 if positive, 0 if negative.
         CFO = division_by_zero_check(recent_operating_cash_flow, recent_total_assets)
         f_score.append(sign_check(CFO))
-        #DeltaROA - net inc / tot asset (this year) - net inc / tot asset (last year) - 1 if pos, 0 if neg
+        #DeltaROA - net inc / tot asset (this year) - net inc / tot asset (last year) - 1 if positive, 0 if negative.
         previous_ROA = division_by_zero_check(previous_net_income, previous_total_assets)
         DeltaROA = ROA - previous_ROA
         f_score.append(sign_check(DeltaROA))
         ACCRUAL = CFO - ROA
-        #Accrual: Cash Flow From Operations Less Return on Assets : 1 if pos, 0 if neg
+        #Accrual: Cash Flow From Operations Less Return on Assets : 1 if positive, 0 if negative.
         f_score.append(sign_check(ACCRUAL))
         #Funding
-        # DeltaLever: Gearing (last year) - Gearing (this year): Score 0 if this year's gearing is higher, 1 otherwise.
+        # DeltaLever: Gearing (last year) - Gearing (this year): 1 if negative, 0 if positive.
         previous_gearing = division_by_zero_check(previous_long_term_debt, previous_avg_total_assets)
         current_gearing = division_by_zero_check(recent_long_term_debt, recent_avg_total_assets)
         DeltaLever = previous_gearing - current_gearing
         f_score.append(sign_check(DeltaLever))
-        # DeltaLiquid: This years current ratio less last years current ratio: Score 1 if postive, else 0
+        # DeltaLiquid: This year's current ratio less last year's current ratio: 1 if positive, 0 if negative.
         Recent_DeltaLiquid = division_by_zero_check(recent_tot_current_assets, recent_total_current_liabilities)
         Previous_DeltaLiquid = division_by_zero_check(previous_tot_current_assets, previous_total_current_liabilities)
         DeltaLiquid = Recent_DeltaLiquid - Previous_DeltaLiquid
         f_score.append(sign_check(DeltaLiquid))
-        #EQ_Offer: Last years number of shares in issue less this years. 1 if positive, else 0.
+        #EQ_Offer: Last year's number of shares in issue less this year's. 1 if positive, 0 if negative.
         EQ_Offer = previous_shares - recent_shares
         f_score.append(sign_check(EQ_Offer))
         #Efficiency
-        #DeltaMargin: Gross Margin (this year) - Gross Margin (last year): 1 if pos, 0 if neg
+        #DeltaMargin: Gross Margin (this year) - Gross Margin (last year): 1 if positive, 0 if negative.
         DeltaMargin = recent_gross_margin - previous_gross_margin
         f_score.append(sign_check(DeltaMargin))
         recent_delta_turn = division_by_zero_check(recent_revenue, previous_total_assets)
         previous_delta_turn = division_by_zero_check(previous_revenue, previous_previous_total_assets)
-        # DeltaTurn: Year's asset turnover ratio less last year's asset turnover ratio. 1 if positive, else 0.
+        # DeltaTurn: Year's asset turnover ratio less last year's asset turnover ratio. 1 if positive, 0 if negative.
         DeltaTurn = recent_delta_turn - previous_delta_turn
         f_score.append(sign_check(DeltaTurn))
         return sum(f_score)
@@ -232,8 +232,8 @@ def parse(driver, stock):
 """
 exception_handling_text_element(str, selenium.webdriver.chrome.webdriver.WebDriver) -> str
 
-checks to see if an exception was thrown when searching for a text formatted element. If no exception was found
-then return text element, else return 0.
+checks to see if an exception was thrown when searching for a text formatted element. If no exception was found,
+then return text element; else return 0.
 
 >>> find_element_by_xpath('//*[@id="tab-profitability"]/table[1]/tbody/tr[6]/td[10]', driver)
 Most recent Gross Margin data
@@ -250,8 +250,8 @@ def exception_handling_text_element(text_element, driver):
 """
 exception_handling_raw_element(str, selenium.webdriver.chrome.webdriver.WebDriver) -> str
 
-checks to see if an exception was thrown when searching for a raw formatted element. If no exception was thrown
-then return raw element, else return zero.
+checks to see if an exception was thrown when searching for a raw formatted element. If no exception was found,
+then return raw element; else return 0.
 
 >>> find_element_by_xpath('//*[@id="data_i1"]/div[@id="Y_5"]', driver)
 Most Recent Revenue
@@ -268,7 +268,7 @@ def exception_handling_raw_element(raw_element, driver):
 float_converter(str) -> float
 
 converts xpath_to_data from a string to float. xpath_to_data equals a number represented as a string, dash, or zero 
-depending on what data was pulled from a given webpage.
+depending on what data was pulled from that particular web page.
 
 >>> float_converter(exception_handling_text_element('//*[@id="financials"]/table/tbody/tr[10]/td[10]', driver))
 2.0
@@ -304,7 +304,11 @@ def division_by_zero_check(numerator, denominator):
 """
 human_like_click(selenium.webdriver.chrome.webdriver.WebDriver, selenium.webdriver.support.wait.WebDriverWait, str) -> None
 
-Waits for the location where the next click is going to take place to appear and then clicks that location.
+Waits for the click location to appear, and clicks that location. This prevents the program from 
+navigating through the website faster than a human could, which in some cases would result in an ip ban. For further 
+information on this subject:
+
+"Web Scraping with Python", by Ryan Mitchell
 
 >>> human_like_click(driver, wait, '//*[@id="keyStatWrap"]/div/ul/li[5]/a')
 None
@@ -329,7 +333,7 @@ def human_like_click(driver, wait, click_xpath):
 """
 sign_check(float) -> int
 
-Checks to see if pos_or_neg is great than 0. If yes, returns a 1 else returns 0.
+Checks to see if pos_or_neg is greater than 0. If yes, returns a 1; Else returns 0.
 
 >>> 2.0
 1
